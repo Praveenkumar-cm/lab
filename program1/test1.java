fb72eb925b5a084ba4faeac338b90ad586679cc1
Praveenkumar-cm <110027914+Praveenkumar-cm@users.noreply.github.com>
2022-09-22 15:08:13 +0530

    Add files via upload

---
+++ program1/test1.java
@@ -0,0 +1,16 @@
+//Author: Praveen kumar
+//Title: basic hello world program
+//quiz : To print the string,int,float,long,boolean
+
+
+class test1
+{
+public static void main(String[]args)
+{
+   System.out.println("Hello world");
+   System.out.println(15);
+   System.out.println(17.5F);
+   System.out.println(12345790987654321L);
+   System.out.println(true);
+}
+}
