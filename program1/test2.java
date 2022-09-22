6f45dcbb63b621917ab68391340ae78863eeeede
Praveenkumar-cm <110027914+Praveenkumar-cm@users.noreply.github.com>
2022-09-22 15:14:24 +0530

    Add files via upload

---
+++ program1/test2.java
@@ -0,0 +1,17 @@
+//Author: Praveen kumar
+//Title: basic program
+//quiz:  printing value1,value2,result 
+
+
+class test2
+{
+  public static void main(String[]arg)
+  {
+        int value1=15;
+	float value2=5.5F;
+	boolean result=true;
+	System.out.println(value1);
+	System.out.println(value2);
+	System.out.println(result);
+  }
+}
