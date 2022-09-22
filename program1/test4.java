b7ee9c5472330171fbb1791e4c88ddd62c7469a2
Praveenkumar-cm <110027914+Praveenkumar-cm@users.noreply.github.com>
2022-09-22 15:20:44 +0530

    Add files via upload

---
+++ program1/test4.java
@@ -0,0 +1,15 @@
+//Author: Praveen kumar
+//Title: boolean operation
+//quiz: dooing AND operation,OR operation
+class test4
+{
+ public static void main(String[]args)
+ {
+  boolean value1=(50<100)&&(10>5)&&(30==30);
+  System.out.println(value1); 
+  boolean value2=(10<20)||(20>15)||(75==10);
+  System.out.println(value2);
+  boolean value3=(30<40)&&(70>80)||(70<80)||(90==90);
+  System.out.println(value3);
+ }
+} 
